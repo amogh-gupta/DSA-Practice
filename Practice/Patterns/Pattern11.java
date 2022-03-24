308db88d150f41aca76a223b2f037d4461108da2
amogh-gupta <amoghgupta002@gmail.com>
2022-03-24 15:25:03 +0530

    Pattern 11

--- Practice/Patterns/Pattern11.java
+++ Practice/Patterns/Pattern11.java
@@ -1,3 +1,15 @@
+// Question URL : https://nados.io/question/pattern-11
+import java.util.Scanner;
 public class Pattern11 {
-    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt(), n = 1;
+        for(int i = 1; i<=num; i++){
+            for(int j = 0; j<i; j++){
+                System.out.print(n + "\t");
+                n++;
+            }
+            System.out.println();
+        }        
+    }
 }
