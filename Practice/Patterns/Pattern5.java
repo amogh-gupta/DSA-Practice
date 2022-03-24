fb40feddf4f61651896966e55b41d2e2652de169
amogh-gupta <amoghgupta002@gmail.com>
2022-03-24 14:48:52 +0530

    Pattern 5

--- Practice/Patterns/Pattern5.java
+++ Practice/Patterns/Pattern5.java
@@ -4,18 +4,23 @@ public class Pattern5 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int num = scn.nextInt();
+        int space = num/2, star = 1;
         for(int i = 0; i<num; i++){
-            int space = i;
-            for(int j = 0; j<num; j++){
-                if(space>0){
-                    System.out.print(" \t");
-                }
-                else{
-                    System.out.print("*\t");
-                }
-                space--;
+            for(int j = 0; j<space; j++){
+                System.out.print(" \t");
+            }
+            for(int k = 0; k<star; k++){
+                System.out.print("*\t");
             }
             System.out.println();
-        }        
+            if(i<num/2){
+                space--;
+                star += 2;
+            }        
+            else{
+                space++;
+                star -= 2;
+            }
+        }
     }
 }
