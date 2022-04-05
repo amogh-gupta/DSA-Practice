7ff4b89a540dd8ab04f71407b970cef3d336a415
amogh-gupta <amoghgupta002@gmail.com>
2022-04-05 23:48:32 +0530

    LeetCode - Guess Number Higher or Lower

---
+++ Practice/19) Guess Number Higher or Lower/GuessNumberHigherorLower.java
@@ -0,0 +1,19 @@
+// Question URL : https://leetcode.com/problems/guess-number-higher-or-lower/ 
+public class GuessNumberHigherorLower {
+    public int guessNumber(int n) {
+        int left = 1, right = n, mid;
+        while(true){
+            mid = left + ((right-left)/2);
+            int num = guess(mid);
+            if (num == -1){
+                right = mid - 1;
+            }
+            else if(num == 1){
+                left = mid + 1;
+            }
+            else{
+                return mid;
+            }
+            }
+    }
+}
