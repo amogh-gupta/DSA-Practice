589aee30c8c8baa987729b5f7e2c2dba6aae43f7
amogh-gupta <amoghgupta002@gmail.com>
2022-04-23 00:44:25 +0530

    Factorial

---
+++ 3) Recursion/4) Factorial/Factorial.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Factorial {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int fac = factorial(n);
+        System.out.println(fac);
+        scn.close();
+    }
+
+    public static int factorial(int n){
+        if(n<=1){
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+
+}
