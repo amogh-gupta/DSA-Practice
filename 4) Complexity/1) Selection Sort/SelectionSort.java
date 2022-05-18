2b4422ef5058d4cb1ccc683c55f4069bd05e9ce9
amogh-gupta <amoghgupta002@gmail.com>
2022-05-18 19:29:30 +0530

    Selection Sort

---
+++ 4) Complexity/1) Selection Sort/SelectionSort.java
@@ -0,0 +1,27 @@
+// Question URL: https://leetcode.com/problems/sort-an-array/submissions/
+import java.util.Scanner;
+class Solution {
+    public static int[] sortArray(int[] nums) {
+        for(int i = 0; i<nums.length-1; i++){
+            int minIdx = i;
+            for(int j = i+1; j<nums.length; j++){
+                if(nums[minIdx]>nums[j]){
+                    minIdx=j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[minIdx];
+            nums[minIdx] = temp;
+        }       
+        return nums;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int[] arr = {5,2,3,1};
+        int[] ans = sortArray(arr);
+        for(int i = 0; i<ans.length; i++){
+            System.out.print(ans[i]+" ");
+        }
+        scn.close();
+    }
+}
