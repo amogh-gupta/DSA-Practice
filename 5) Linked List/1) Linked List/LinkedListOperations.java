19358ff0273155869a35a8ae031bbd6627bcbf52
amogh-gupta <amoghgupta002@gmail.com>
2022-05-24 23:46:04 +0530

    Linked List - add new node at last

---
+++ 5) Linked List/1) Linked List/LinkedListOperations.java
@@ -0,0 +1,53 @@
+// Question URL : https://nados.io/content/linked-lists-for-beginners
+public class LinkedListOperations {
+    public static class Node{
+        int data;
+        Node next;
+    }
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+
+        int size(){
+            return this.size;
+        }
+
+        void addLast(int val){ // Adding node to the last in the link list
+            Node node = new Node();
+            node.data = val;
+
+            if(this.size == 0){
+                this.head = node;
+                this.tail = node;
+            }
+            else{
+                this.tail.next = node;
+                this.tail = node;
+            }
+            this.size++;
+        }
+
+        
+
+        void displayLinkedList(){
+            Node node = this.head;
+            System.out.print("LinkList is :- ");
+            while(node != null){
+                System.out.print(node.data+" ");
+                node = node.next;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        LinkedList l1 = new LinkedList();
+        l1.addLast(30);
+        l1.addLast(40);
+        l1.addLast(50);
+
+        System.out.println("Size :- " + l1.size);
+
+        l1.displayLinkedList();
+    }    
+}
