06cf5d2f0132a5b4a3001646e6ba6434e0d1d48b
amogh-gupta <amoghgupta002@gmail.com>
2022-05-27 21:17:50 +0530

    Leet Code - 237. Delete Node in a Linked List

--- 5) Linked List/8) Delete Node in a Linked List/Solution.java
+++ 5) Linked List/8) Delete Node in a Linked List/Solution.java
@@ -1,5 +1,10 @@
 // QUESTION URL : - https://leetcode.com/problems/delete-node-in-a-linked-list
 public class Solution {
+    public class ListNode {
+            int val;
+            ListNode next;
+            ListNode(int x) { val = x; }
+        }
     public void deleteNode(ListNode node) {
         while(node.next!=null){
             node.val = node.next.val;
