4128597ccdd1e357a30655ab2435e13145a6582d
amogh-gupta <amoghgupta002@gmail.com>
2022-05-28 23:54:15 +0530

    GFG - Find length of Loop - Approach 2

---
+++ 5) Linked List/12) Find length of Loop/Solution2.java
@@ -0,0 +1,45 @@
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+//Function should return the length of the loop in LL.
+
+class Solution2 {
+    //Function to find the length of a loop in the linked list.
+    static int countNodesinLoop(Node head)
+    {
+        //Add your code here.
+        int index = 0;
+        if(head == null || head.next == null){
+            return index;
+        }
+        Node slow, fast, curr;
+        curr = slow = fast = head;
+        boolean flag = false;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag) return index;
+        
+        // while(slow!= curr){
+        //     slow = slow.next;
+        //     curr = curr.next;
+        // }
+        while(true){
+            slow = slow.next;
+            index++;
+            // if(slow == curr){
+            if(slow == fast){
+                return index;
+            }
+        }
+    }
+}
