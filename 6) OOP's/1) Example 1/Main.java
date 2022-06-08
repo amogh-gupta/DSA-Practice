ff45e4bc8d173f10cd49ba20ed3d8db8443861c8
amogh-gupta <amoghgupta002@gmail.com>
2022-06-08 22:41:13 +0530

    OOP's Example 1

---
+++ 6) OOP's/1) Example 1/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static class Person{
+        int age;
+        String name;
+    
+        public void print(){
+            System.out.println(this.name + " - " + this.age);
+        }
+    }
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.age = 23;
+        p1.name = "Amogh";
+        p1.print();
+    }
+}
