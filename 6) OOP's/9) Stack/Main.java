1d84f3484cfe35992c706d32df1ae879b2c05326
amogh-gupta <amoghgupta002@gmail.com>
2022-06-09 23:10:28 +0530

    STACK - LIFO - Last In First Out

---
+++ 6) OOP's/9) Stack/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        
+        System.out.println(st.size() + " : " + st);
+        
+        st.push(10);
+        System.out.println(st.size() + " : " + st);
+        
+        st.push(20);
+        System.out.println(st.size() + " : " + st);
+        
+        st.push(30);
+        System.out.println(st.size() + " : " + st);
+        
+        st.push(40);
+        
+        for(int ele : st){
+            System.out.print(ele + " -> ");
+        }
+        System.out.println();
+
+        System.out.println(st);
+
+        System.out.println(st.pop() + " : " + st);
+
+        System.out.println(st.peek() + " : " + st);
+    }   
+         
+}
