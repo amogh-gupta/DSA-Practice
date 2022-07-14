1fbdd95d7ac2cad1a573d6771cbd861db3d86b4a
amogh-gupta <amoghgupta002@gmail.com>
2022-07-14 22:29:25 +0530

    Genetric Tree - Structure and Displaying

--- 7) Trees/1) Generic Tree/1) Structure/Main.java
+++ 7) Trees/1) Generic Tree/1) Structure/Main.java
@@ -36,9 +36,21 @@ public class Main {
         return root;
     }
 
+    public static void display(Node node) {
+        System.out.print(node.data + " -> ");
+        for(Node t : node.children){
+            System.out.print(t.data + " ");
+        }
+        System.out.println(".");
+        
+        for(Node t : node.children){
+            display(t);
+        }
+    }
     public static void main(String[] args){
         Integer []arr = {10, 20, 50, null, 60, null, null, 30, 70, 90, null, 100, null, 110, null, null, null, 40, 80, null, null, null};
         Node root = construct(arr);
+        display(root);
     }
 }
 
